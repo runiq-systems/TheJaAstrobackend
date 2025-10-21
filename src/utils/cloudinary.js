8edbac09a24a09a60ceac287b00fd0183d9d2de3
hindustantech <kumarrupees505@gmail.com>
2025-10-21 15:26:18 +0530

    add

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,6 @@
 import { v2 as cloudinary } from 'cloudinary';
 import fs from 'fs';
-import { ApiError } from './ApiError';
+import { ApiError } from './ApiError.js';
 // Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
