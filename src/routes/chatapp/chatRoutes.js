fa6ce8d41cc5bf18673fac14abb87460a02e4704
OutBreak778 <mishran.2003@gmail.com>
2025-10-21 23:15:31 +0530

    changes in userId of authController

--- src/routes/chatapp/chatRoutes.js
+++ src/routes/chatapp/chatRoutes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import {
-    createOrGetAOneOnOneChat,
-    deleteOneOnOneChat,
-    searchAvailableUsers,
-    markMessageAsRead,
-    getAllChats
+  createOrGetAOneOnOneChat,
+  deleteOneOnOneChat,
+  searchAvailableUsers,
+  markMessageAsRead,
+  getAllChats,
 } from "../../controllers/chatapp/chatController.js";
 import {
-    getAllMessages,
-    sendMessage,
-    deleteMessage,
-    reactToMessage
+  getAllMessages,
+  sendMessage,
+  deleteMessage,
+  reactToMessage,
 } from "../../controllers/chatapp/messageController.js";
 import { authMiddleware } from "../../middleware/authmiddleware.js";
 import { upload } from "../../middleware/multer.js";
@@ -21,30 +21,24 @@ const router = express.Router();
 router.use(authMiddleware);
 
 // Chat routes
-router.route("/one-on-one")
-    .post(createOrGetAOneOnOneChat);
+router.route("/one-on-one").post(createOrGetAOneOnOneChat);
 
-router.route("/search-users")
-    .get(searchAvailableUsers);
+router.route("/search-users").get(searchAvailableUsers);
 
-router.route("/")
-    .get(getAllChats);
+router.route("/").get(getAllChats);
 
-router.route("/:chatId/mark-read")
-    .put(markMessageAsRead);
+router.route("/:chatId/mark-read").put(markMessageAsRead);
 
-router.route("/:chatId")
-    .delete(deleteOneOnOneChat);
+router.route("/:chatId").delete(deleteOneOnOneChat);
 
 // Message routes
-router.route("/:chatId/messages")
-    .get(getAllMessages)
-    .post(upload.array("media", 10), sendMessage); // Allow up to 10 files
+router
+  .route("/:chatId/messages")
+  .get(getAllMessages)
+  .post(upload.array("media", 10), sendMessage); // Allow up to 10 files
 
-router.route("/messages/:messageId")
-    .delete(deleteMessage);
+router.route("/messages/:messageId").delete(deleteMessage);
 
-router.route("/messages/:messageId/react")
-    .put(reactToMessage);
+router.route("/messages/:messageId/react").put(reactToMessage);
 
 export default router;
