597702c97584a84843e7cafbf83a7ff43c1c34f0
hindustantech <kumarrupees505@gmail.com>
2025-10-24 00:35:52 +0530

    add

--- src/socket/index.js
+++ src/socket/index.js
@@ -146,7 +146,7 @@ export const initializeSocketIO = (io) => {
 
       // Verify JWT
       const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-      const user = await User.findById(decoded?._id).select("-password -refreshToken");
+      const user = await User.findById(decoded?.id).select("-password -refreshToken");
       console.log("decoded", decoded)
       console.log("Decoded user :", user);
 
