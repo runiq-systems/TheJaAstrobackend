2fa0e0ae19c5735cd348af07e79b47d538114b3e
hindustantech <kumarrupees505@gmail.com>
2025-10-26 22:09:13 +0530

    add

--- src/middleware/ratelimiter.js
+++ src/middleware/ratelimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const rateLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 25,
+  max: 5000,
   message: {
     success: false,
     message: "Too many requests from this source. Please try again after 10 minutes.",
