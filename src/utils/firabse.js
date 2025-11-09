0d3cfe87828b6864a065d98b3811fa5c0aa646b3
hindustantech <kumarrupees505@gmail.com>
2025-11-09 22:33:27 +0530

    add

--- src/utils/firabse.js
+++ src/utils/firabse.js
@@ -1,19 +1,19 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
-import logger from './logger.js';
 dotenv.config();
 
 // Parse the service account JSON from environment variable
 // console.log("process.env.serviceAccount",process.env.serviceAccount)
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE);
+const serviceAccount = JSON.parse(process.env.serviceAccount);
 
 // Fix private_key (replace escaped "\n" with actual newlines)
 serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
-logger.info('Firebase init Done',serviceAccount)
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
+
+console.log("Firebase Admin SDK initialized with service account",serviceAccount);
 
 export default admin;
