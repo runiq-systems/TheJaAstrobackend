ec6a80e0d6e6445991ed8f1980e93d45f7cc9552
OutBreak778 <mishran.2003@gmail.com>
2025-12-04 00:17:40 +0530

    added Call as non-default in webrtc.service.js

--- src/webrtc/webrtc.service.js
+++ src/webrtc/webrtc.service.js
@@ -1,9 +1,9 @@
 import { User } from "../models/user.js";
 import NotificationService from "./notification.service.js";
 import logger from "../utils/logger.js";
-import Call from "../models/calllogs/call.js";
 import admin from "../utils/firabse.js";
 import { CallNotificationService } from "./notification.js";
+import { Call } from "../models/calllogs/call.js";
 
 export class WebRTCService {
   constructor(io) {
