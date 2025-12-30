5049b30692152277dc362437da35d060dd345ee4
OutBreak778 <mishran.2003@gmail.com>
2025-12-30 16:22:30 +0530

    nothing changes

--- src/routes/chatapp/chatRoutes.js
+++ src/routes/chatapp/chatRoutes.js
@@ -16,6 +16,7 @@ import {
 } from "../../controllers/chatapp/messageController.js";
 import { authMiddleware } from "../../middleware/authmiddleware.js";
 import { upload } from "../../middleware/multer.js";
+import { testSendNotification } from "../../controllers/chatapp/chatSessionController.js";
 
 const router = express.Router();
 
@@ -44,5 +45,7 @@ router
 router.route("/messages/:messageId").delete(deleteMessage);
 
 router.route("/messages/:messageId/react").put(reactToMessage);
+
+router.post("/test/notification", testSendNotification)
 
 export default router;
